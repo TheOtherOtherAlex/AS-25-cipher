bd52c9c21a95b104795ddf7fdf34c6887230f3bc
Alex Zhang <118483351+TheOtherOtherAlex@users.noreply.github.com>
2024-04-17 11:36:19 -0500

    Update Main.java

--- intellijDecryption/src/main/java/org/example/Main.java
+++ intellijDecryption/src/main/java/org/example/Main.java
@@ -8,8 +8,6 @@ public class Main {
         double intitialTime = System.currentTimeMillis();
 
         System.out.println("Hello and welcome!");
-        System.out.println('z'-'a');
-        //EncryptedTextExtractor encryptedText = new EncryptedTextExtractor("src/main/resources/LongEncryptedText");
 
         //CreateAdditiveArray createAdditiveArray = new CreateAdditiveArray();
         //createAdditiveArray.createArray("src/main/resources/NewAdditiveBook");
@@ -22,26 +20,5 @@ public class Main {
         DecryptNewCipher decryptNewCipher = new DecryptNewCipher(encryptNewText.getMappings(), encryptNewText.getAdditiveBook(),encryptNewText.getFullyEncryptedText());
         System.out.println(decryptNewCipher.decryptCipher());
 
-
-
-
-
-
-       //DecryptionKeyExtractor decrpytionText = new DecryptionKeyExtractor("src/main/resources/codebook_agnes.txt");
-
-
-//       FastDecryptor fastDecryptor = new FastDecryptor(encryptedText.encryptedText,"src/main/resources/CodebookAgnesFieldEncrypt.txt");
-//       System.out.println(fastDecryptor.decryptFile());
-
-
-       //DecryptText decryptText = new DecryptText(encryptedText.encryptedText,decrpytionText.getMappings());
-//       System.out.println(decryptText.decryptedText());
-
-//
-//       TextSorter textSorter = new TextSorter(decrpytionText);
-//       System.out.println("go!");
-//       textSorter.writeSortedToFile("src/main/resources/CodebookAgnesFieldDecrypt.txt",false);
-//       textSorter.writeSortedToFile("src/main/resources/CodebookAgnesFieldEncrypt.txt",true);
-//       System.out.println("done!");
     }
 }
